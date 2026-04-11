f518abca7db470c7dd5b4a299c420476d09c7f28
Keshav Poojary <richiehydra22@gmail.com>
2026-04-12 00:30:11 +0530

    sigterm shutdown and ci enhancements

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Swagger } from './swagger';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +9,21 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
+
+  process.on('SIGTERM', () => {
+    Logger.log('SIGTERM received, shutting down gracefully...');
+    app
+      .close()
+      .then(() => {
+        Logger.log('Application closed successfully.');
+        process.exit(0);
+      })
+      .catch((err) => {
+        Logger.error('Error during shutdown:', err);
+        process.exit(1);
+      });
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
